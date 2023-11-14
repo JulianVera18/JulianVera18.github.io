b9f7fab4b83f87b691d07bafbed483eaff88d48a
Julián Vera <85370987+JulianVera18@users.noreply.github.com>
2023-11-14 13:49:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ function convertir(){
     resultado = cantidad * Lvalue;
     creditos = cantidad * 50;
     console.log(resultado);
-    document.getElementById("resultado").innerText = "Resultado: " + resultado + " USDT\n" + creditos + " creditos";
+    document.getElementById("resultado").innerText = "Resultado: " + resultado + " USDT\n" + creditos + " créditos";
   }else if (opcion === "usdtToLingos"){
     resultado = cantidad / Lvalue;
     creditos = resultado * 50;
     console.log(resultado);
-    document.getElementById("resultado").innerText = "Resultado: " + resultado + " Lingos\n" + creditos + " creditos";
+    document.getElementById("resultado").innerText = "Resultado: " + resultado + " Lingos\n" + creditos + " créditos";
+  }else if (opcion === "creditsToUSDT"){
+    resultado = (cantidad/50) * Lvalue;
+    creditos = cantidad / 50;
+    console.log(resultado);
+    document.getElementById("resultado").innerText = "Resultado: " + resultado + " USDT\n" + creditos + " Lingos";
   }
 }
 
@@ -25,7 +30,9 @@ function cambiarLabel(){
 
   if (opcionSeleccionada === "lingosToUSDT") {
     label.innerText = "Cantidad de Lingos:";
-  } else {
+  } else if (opcionSeleccionada === "usdtToLingos") {
     label.innerText = "Cantidad de USTD:";
+  } else if (opcionSeleccionada === "creditsToUSDT"){
+    label.innerText = "Cantidad de créditos:";
   }
 }
